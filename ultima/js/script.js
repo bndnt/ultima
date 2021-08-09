c2d648d330b2e21bbd37111d1e5d5d92725fec8a
bndnt <l.makushchenko@gmail.com>
2021-08-09 16:15:27 +0300

    sadasdasd2

--- ultima/js/script.js
+++ ultima/js/script.js
@@ -1,10 +1,9 @@
-
 $(document).on('click', '.course-for__det-top ', function (e) {
     if ($(this).parents('.answer__item').hasClass('active')) {
         // console.log(1)
         $(this).parents('.answer__item').removeClass('active');
         $(this).siblings('.course-for__det-hidden').hide(300);
-    }else {
+    } else {
         $(this).parents('.course-for__detail-block').siblings().find('.answer__item').removeClass('active');
         $(this).parents('.course-for__detail-block').siblings().find('.course-for__det-hidden').slideUp();
         $(this).parents('.answer__item').addClass('active');
@@ -13,19 +12,19 @@ $(document).on('click', '.course-for__det-top ', function (e) {
 });
 
 var hideBlocks = document.querySelectorAll('.course-for__detail-block-not-visible')
-document.querySelector('.jc-course-for__open-more').addEventListener('click',function (){
+document.querySelector('.jc-course-for__open-more').addEventListener('click', function () {
     this.classList.toggle('active')
-    for (item of hideBlocks){
+    for (item of hideBlocks) {
         item.classList.toggle('active')
     }
 })
 
-$(document).on('click', '.get-to-know__top-item', function (e){
+$(document).on('click', '.get-to-know__top-item', function (e) {
     if ($(this).parents('.js-get-to-know__point').hasClass('active')) {
         // console.log(1)
         $(this).parents('.js-get-to-know__point').removeClass('active');
         $(this).siblings('.get-to-know__hide-block').hide(300);
-    }else {
+    } else {
         // console.log(2)
         $(this).parents('.js-get-to-know__point').siblings().removeClass('active');
         $(this).parents('.js-get-to-know__point').siblings().find('.get-to-know__hide-block').slideUp();
@@ -35,20 +34,19 @@ $(document).on('click', '.get-to-know__top-item', function (e){
 });
 
 
-
 // var openHide2 = document.querySelectorAll('.get-to-know__point-hide');
-document.querySelector('.js-get-to-know__open-more').addEventListener('click', function (){
+document.querySelector('.js-get-to-know__open-more').addEventListener('click', function () {
     this.classList.toggle('active')
     document.querySelector('.js-get-to-know__hide-whole-block').classList.toggle('active')
 })
 
 
-$(document).on('click', '.course-works__title', function (e){
+$(document).on('click', '.course-works__title', function (e) {
     if ($(this).parents('.course-works__info-flex').hasClass('active')) {
         // console.log(1)
         $(this).parents('.course-works__info-flex').removeClass('active');
         $(this).siblings('.course-works__text').hide(300);
-    }else {
+    } else {
         // console.log(2)
         $(this).parents('.course-works__info-flex').siblings().removeClass('active');
         $(this).parents('.course-works__info-flex').siblings().find('.course-works__text').slideUp();
@@ -60,7 +58,7 @@ $(document).on('click', '.course-works__title', function (e){
 var swiper1 = new Swiper(".mentorsSwiper", {
     slidesPerView: 'auto',
     spaceBetween: 80,
-    speed:1000,
+    speed: 1000,
     mousewheel: true,
     keyboard: true,
     breakpoints: {
@@ -82,12 +80,12 @@ var swiper1 = new Swiper(".mentorsSwiper", {
 
 if ($(window).width() < 769) {
 
-    $(document).on('click', '.faq__question', function (e){
+    $(document).on('click', '.faq__question', function (e) {
         if ($(this).parents('.faq__flex').hasClass('active')) {
             // console.log(1)
             $(this).parents('.faq__flex').removeClass('active');
             $(this).siblings('.faq__ask').slideUp();
-        }else {
+        } else {
             // console.log(2)
             $(this).parents('.faq__flex').siblings().removeClass('active');
             $(this).parents('.faq__flex').siblings().find('.faq__ask').slideUp();
@@ -100,13 +98,30 @@ AOS.init({
     duration: 1500,
     once: true,
 });
-if ($(window).width() < 769){
+if ($(window).width() < 769) {
     AOS.init({
         once: true,
     });
 
 }
-// AOS.init({
-// // once: true,);
+
 
 $("#second").inputmask("+55 (99) 99999-9999");
+
+if ($(window).width() > 769) {
+    $(".faq__flex").hover(function () {
+        $(this).find('.faq__ask').fadeIn(800);
+        console.log(1)
+    }, function () {
+        $(this).find('.faq__ask').hide();
+        console.log(2)
+    });
+}
+// if ($(window).width() > 769){
+//     $('.faq__question').mouseover(function(){
+//         if($(this).hasClass(''))
+//         $('.faq__ask').css({
+//             'opacity':'1', 'visibility':'visible'
+//         });
+//     })
+// }
